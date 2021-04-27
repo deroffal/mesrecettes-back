0cb5a3a70279f8230125a199695d2c0fcfbee8e3
deroffal <32491733+deroffal@users.noreply.github.com>
2021-04-27 21:17:07 +0200

    init

---
+++ src/main/kotlin/fr/deroffal/mesrecettesback/MesrecettesBackApplication.kt
@@ -0,0 +1,11 @@
+package fr.deroffal.mesrecettesback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MesrecettesBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<MesrecettesBackApplication>(*args)
+}
