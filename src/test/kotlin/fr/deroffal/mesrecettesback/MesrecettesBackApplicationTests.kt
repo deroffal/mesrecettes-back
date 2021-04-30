8a544d2d98c1bb8976f87b0b063a9fdd87a4b1c6
deroffal <32491733+deroffal@users.noreply.github.com>
2021-04-30 18:14:08 +0200

    Initialisation base de données :
    * liquibase
    * testcontainers
    
    NOTE : Liquibase ne peut utiliser qu'une connexion jdbc pour le moment.

--- src/test/kotlin/fr/deroffal/mesrecettesback/MesrecettesBackApplicationTests.kt
+++ src/test/kotlin/fr/deroffal/mesrecettesback/MesrecettesBackApplicationTests.kt
@@ -2,8 +2,10 @@ package fr.deroffal.mesrecettesback
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MesrecettesBackApplicationTests {
 
 	@Test
