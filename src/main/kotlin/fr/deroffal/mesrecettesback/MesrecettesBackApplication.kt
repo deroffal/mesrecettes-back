067be2c169c4fd6f29c6c6e1efff026f65f39da4
deroffal <32491733+deroffal@users.noreply.github.com>
2021-07-20 18:40:11 +0200

    remove commented code

--- src/main/kotlin/fr/deroffal/mesrecettesback/MesrecettesBackApplication.kt
+++ src/main/kotlin/fr/deroffal/mesrecettesback/MesrecettesBackApplication.kt
@@ -1,12 +1,7 @@
 package fr.deroffal.mesrecettesback
 
-import com.mongodb.reactivestreams.client.MongoClient
-import com.mongodb.reactivestreams.client.MongoClients
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
 
@@ -17,13 +12,3 @@ class MesrecettesBackApplication
 fun main(args: Array<String>) {
     runApplication<MesrecettesBackApplication>(*args)
 }
-
-////@Configuration
-//@EnableReactiveMongoRepositories
-//class MongoReactiveApplication : AbstractReactiveMongoConfiguration() {
-//
-//    @Bean
-//    fun mongoClient(): MongoClient = MongoClients.create()
-//
-//    override fun getDatabaseName() = "mesrecettesdb"
-//}
